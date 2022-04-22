5b230cfd8673501b366fac86b0252a9feaa8703d
Daniil Kotov <daniel0k@mail.ru>
2022-04-22 15:48:01 +0200

    initial commit

---
+++ online-store/src/app/store/summary-form/summary-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-summary-form',
+  templateUrl: './summary-form.component.html',
+  styleUrls: ['./summary-form.component.less']
+})
+export class SummaryFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
